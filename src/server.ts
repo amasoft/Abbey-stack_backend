4222006db391060670cca232d74cd286f471a303
Patrick <amadifaraday@gmail.com>
2025-10-28 06:00:54 +0100

    fix: fixed Port issues

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { checkDatabaseConnection } from "./config/Connection";
 const PORT =4000
-app.listen(PORT,async()=>{
+app.listen(process.env.PORT || PORT,async()=>{
   await  checkDatabaseConnection()
     console.log(`Server Running on http://localhost:${PORT}`)
     
